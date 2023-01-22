fd8379741c3580970fa28c813d07de81b4c4f99e
Pawel <pawel@pawel.com>
2023-01-22 09:34:06 +0100

    add sortedTrips and tripSort controller but not working 2.

--- src/main/java/pl/sda/zdjavapol119/Travel_Agency_My_Dream_Holidays/model/Role.java
+++ src/main/java/pl/sda/zdjavapol119/Travel_Agency_My_Dream_Holidays/model/Role.java
@@ -3,10 +3,8 @@ package pl.sda.zdjavapol119.Travel_Agency_My_Dream_Holidays.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Data
@@ -18,7 +16,7 @@ public class Role {
     private Long id;
     private String name;
 
-//    @ManyToMany(mappedBy = "roles")
-//    private List<User> users;
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
 
 }
